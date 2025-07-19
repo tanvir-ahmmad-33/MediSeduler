5ab1ded47f264a19e58aa53c9985602d6d8ab261
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2025-07-19 22:48:35 +0600

    works on home page navbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+@include('common.head')
+<body>
+    <!-- navbar section -->
+    @include('common.home_navbar')
+
+
+    <!-- Bootstrap js cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
